e2f807228fa4a7670ce4dfcf28955f95c59348ee
lschmid <lschmid@mit.edu>
2024-01-09 14:49:01 -0500

    more coloring tools

--- include/spark_dsg/mesh.h
+++ include/spark_dsg/mesh.h
@@ -49,6 +49,11 @@ struct Color {
 
   Color() = default;
   Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255) : r(r), g(g), b(b), a(a) {}
+
+  bool operator==(const Color& other) const {
+    return r == other.r && g == other.g && b == other.b && a == other.a;
+  }
+  bool operator!=(const Color& other) const { return !(*this == other); }
 };
 
 class Mesh {
