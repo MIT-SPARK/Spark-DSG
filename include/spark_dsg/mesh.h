fdcc3993a6b427064b9478361245d8010e32c749
lschmid <lschmid@mit.edu>
2024-01-09 08:46:44 -0500

    add color constructors

--- include/spark_dsg/mesh.h
+++ include/spark_dsg/mesh.h
@@ -42,10 +42,13 @@
 namespace spark_dsg {
 
 struct Color {
-  uint8_t r;
-  uint8_t g;
-  uint8_t b;
-  uint8_t a;
+  uint8_t r = 0;
+  uint8_t g = 0;
+  uint8_t b = 0;
+  uint8_t a = 255;
+
+  Color() = default;
+  Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255) : r(r), g(g), b(b), a(a) {}
 };
 
 class Mesh {
