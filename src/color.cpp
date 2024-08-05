91bf2e24b2da475294cd532153d794ee3f50e5f1
Nathan Hughes <nathan.h.hughes@gmail.com>
2024-08-05 10:30:15 -0400

    Feature/more colormaps (#14)
    * add room colormap and cleanup ironbow
    
    * add conversion to hsv and hls
    
    * move colormaps to be functions
    
    * move colormaps to new file
    
    * add 150 class map and divergent map
    
    * clean up discrete colormaps
    
    * better sort colormaps
    
    * improve divergent docstring
    
    ---------
    
    Co-authored-by: Nathan Hughes <na26933@mit.edu>

--- src/color.cpp
+++ src/color.cpp
@@ -9,6 +9,83 @@ std::random_device rd;
 std::mt19937 gen(rd());
 std::uniform_int_distribution<uint8_t> dis(0, 255);
 
+namespace {
+
+struct HueInfo {
+  float max = 0.0f;
+  float min = 1.0f;
+  float hue = 0.0;
+};
+
+std::array<float, 3> rgbFromChromaHue(float chroma, float hue) {
+  const float hue_prime = hue * 6.0f;
+  const float x = chroma * (1.0f - std::abs(std::fmod(hue_prime, 2) - 1.0f));
+  if (hue_prime < 1) {
+    return {chroma, x, 0};
+  } else if (hue_prime < 2) {
+    return {x, chroma, 0};
+  } else if (hue_prime < 3) {
+    return {0, chroma, x};
+  } else if (hue_prime < 4) {
+    return {0, x, chroma};
+  } else if (hue_prime < 5) {
+    return {x, 0, chroma};
+  }
+  return {chroma, 0, x};
+}
+
+// see https://docs.opencv.org/3.4/de/d25/imgproc_color_conversions.html for details
+HueInfo getHue(const Color& color) {
+  // TODO(nathan) think about tie-breaking
+  std::array<float, 3> rgb{color.r / 255.0f, color.g / 255.0f, color.b / 255.0f};
+
+  HueInfo info;
+  size_t max_idx = 0;
+  for (size_t c = 0; c < 3; ++c) {
+    if (rgb[c] > info.max) {
+      info.max = rgb[c];
+      max_idx = c;
+    }
+
+    if (rgb[c] < info.min) {
+      info.min = rgb[c];
+    }
+  }
+
+  if (color.r == color.g && color.g == color.b) {
+    // maps to central axis, but we define it as 0.0
+    info.hue = 0.0;
+  } else if (max_idx == 0 && color.r == color.g) {
+    // should be equidistant between red and green
+    info.hue = 1.0f / 6.0f;
+  } else if (max_idx == 0 && color.r == color.b) {
+    // should be equidistant between blue and red
+    info.hue = 5.0f / 6.0f;
+  } else if (max_idx == 1 && color.g == color.b) {
+    // should be equidistant between green and blue
+    info.hue = 0.5f;
+  } else {
+    const auto l_idx = (max_idx + 1) % 3;
+    const auto r_idx = (max_idx + 2) % 3;
+    const auto chroma = info.max - info.min;
+    info.hue = (rgb[l_idx] - rgb[r_idx]) / (2.0f * chroma) + max_idx;
+    info.hue /= 3.0;
+  }
+
+  if (info.hue < 0.0f) {
+    info.hue += 1.0f;
+  }
+
+  return info;
+}
+
+// remap unit-range color value to nearest uint8_t value
+inline uint8_t fromUnitRange(float value) {
+  return static_cast<uint8_t>(std::clamp(std::round(255.0f * value), 0.0f, 255.0f));
+}
+
+}  // namespace
+
 bool Color::operator==(const Color& other) const {
   return r == other.r && g == other.g && b == other.b && a == other.a;
 }
@@ -38,21 +115,8 @@ Color Color::blend(const Color& other, float weight) const {
 
 Color Color::random() { return Color(dis(gen), dis(gen), dis(gen), dis(gen)); }
 
-std::array<float, 3> rgbFromChromaHue(float chroma, float hue) {
-  const float hue_prime = hue * 6.0f;
-  const float x = chroma * (1.0f - std::abs(std::fmod(hue_prime, 2) - 1.0f));
-  if (hue_prime < 1) {
-    return {chroma, x, 0};
-  } else if (hue_prime < 2) {
-    return {x, chroma, 0};
-  } else if (hue_prime < 3) {
-    return {0, chroma, x};
-  } else if (hue_prime < 4) {
-    return {0, x, chroma};
-  } else if (hue_prime < 5) {
-    return {x, 0, chroma};
-  }
-  return {chroma, 0, x};
+std::array<float, 4> Color::toUnitRange() const {
+  return {r / 255.0f, g / 255.0f, b / 255.0f, a / 255.0f};
 }
 
 Color Color::fromHSV(float hue, float saturation, float value) {
@@ -62,9 +126,18 @@ Color Color::fromHSV(float hue, float saturation, float value) {
   const float chroma = value * saturation;
   const auto [r1, g1, b1] = rgbFromChromaHue(chroma, hue);
   const float m = value - chroma;
-  return Color(static_cast<uint8_t>((r1 + m) * 255),
-               static_cast<uint8_t>((g1 + m) * 255),
-               static_cast<uint8_t>((b1 + m) * 255));
+  return Color(fromUnitRange(r1 + m), fromUnitRange(g1 + m), fromUnitRange(b1 + m));
+}
+
+// see https://docs.opencv.org/3.4/de/d25/imgproc_color_conversions.html for details
+std::array<float, 3> Color::toHSV() const {
+  const auto info = getHue(*this);
+
+  std::array<float, 3> hsv;
+  hsv[0] = info.hue;
+  hsv[1] = info.max == 0.0f ? 0.0f : static_cast<float>(info.max - info.min) / info.max;
+  hsv[2] = info.max;
+  return hsv;
 }
 
 Color Color::fromHLS(float hue, float luminance, float saturation) {
@@ -74,76 +147,26 @@ Color Color::fromHLS(float hue, float luminance, float saturation) {
   const float chroma = (1.0f - std::abs(2.0f * luminance - 1.0f)) * saturation;
   const auto [r1, g1, b1] = rgbFromChromaHue(chroma, hue);
   const float m = luminance - chroma / 2.0f;
-  return Color(static_cast<uint8_t>((r1 + m) * 255),
-               static_cast<uint8_t>((g1 + m) * 255),
-               static_cast<uint8_t>((b1 + m) * 255));
+  return Color(fromUnitRange(r1 + m), fromUnitRange(g1 + m), fromUnitRange(b1 + m));
 }
 
-Color Color::gray(float value) {
-  value = std::clamp(value, 0.0f, 1.0f);
-  return Color(static_cast<uint8_t>(value * 255),
-               static_cast<uint8_t>(value * 255),
-               static_cast<uint8_t>(value * 255));
-}
+// see https://docs.opencv.org/3.4/de/d25/imgproc_color_conversions.html for details
+std::array<float, 3> Color::toHLS() const {
+  const auto info = getHue(*this);
 
-Color Color::quality(float value) {
-  value = std::clamp(value, 0.0f, 1.0f);
-  Color color;
-  if (value > 0.5f) {
-    color.r = (1.f - value) * 2 * 255;
-    color.g = 255;
+  std::array<float, 3> hls;
+  hls[0] = info.hue;
+  hls[1] = (info.max + info.min) / 2.0f;
+  if (hls[1] == 0.0f || hls[1] == 1.0f) {
+    hls[2] = 0.0f;
+  } else if (hls[1] < 0.5f) {
+    hls[2] = (info.max - info.min) / (info.max + info.min);
   } else {
-    color.r = 255;
-    color.g = value * 2 * 255;
+    hls[2] = (info.max - info.min) / (2.0f - info.max - info.min);
   }
-  return color;
+
+  return hls;
 }
-
-Color Color::spectrum(float value, const std::vector<Color>& colors) {
-  if (colors.empty()) {
-    return Color::black();
-  }
-  value = std::clamp(value, 0.0f, 1.0f);
-  const size_t num_steps = colors.size() - 1;
-  const size_t index = static_cast<size_t>(value * num_steps);
-  if (index >= num_steps) {
-    return colors.at(num_steps);
-  }
-  const float weight = value * num_steps - index;
-  return colors.at(index).blend(colors.at(index + 1), weight);
-}
-
-Color Color::ironbow(float value) { return spectrum(value, ironbow_colors_); }
-
-Color Color::rainbow(float value) {
-  return fromHLS(std::clamp(value, 0.0f, 1.0f), 0.5f, 1.0f);
-}
-
-/**
- * @brief Map a potentially infinite number of ids to a never repeating pattern in [0,
- * 1].
- */
-float exponentialOffsetId(size_t id, size_t ids_per_revolution) {
-  const size_t revolution = id / ids_per_revolution;
-  const float progress_along_revolution =
-      std::fmod(static_cast<float>(id) / ids_per_revolution, 1.f);
-  float offset = 0.0f;
-  if (ids_per_revolution < id + 1u) {
-    const size_t current_episode = std::floor(std::log2(revolution));
-    const size_t episode_start = std::exp2(current_episode);
-    const size_t current_subdivision = revolution - episode_start;
-    const float subdivision_step_size = 1.0f / (ids_per_revolution * 2 * episode_start);
-    offset = (2.0f * current_subdivision + 1) * subdivision_step_size;
-  }
-  return progress_along_revolution + offset;
-}
-
-Color Color::rainbowId(size_t id, size_t ids_per_revolution) {
-  return Color::rainbow(exponentialOffsetId(id, ids_per_revolution));
-}
-
-const std::vector<Color> Color::ironbow_colors_ = {
-    {0, 0, 0}, {145, 20, 145}, {255, 138, 0}, {255, 230, 40}, {255, 255, 255}};
 
 std::ostream& operator<<(std::ostream& out, const Color& color) {
   out << "[RGBA: " << static_cast<int>(color.r) << ", " << static_cast<int>(color.g)
