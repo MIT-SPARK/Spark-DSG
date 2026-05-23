2aaef623e8b324a5aa43becad7a162c7f39904b5
Nathan Hughes <nathan.h.hughes@gmail.com>
2026-05-23 13:13:40 -0400

    add new command-line command to update scene graph files

--- python/spark_dsg/__main__.py
+++ python/spark_dsg/__main__.py
@@ -1,7 +1,9 @@
 """Entry points for spark_dsg."""
 
 import click
+import pathlib
 
+import spark_dsg as dsg
 import spark_dsg.commands.visualize as visualize
 import spark_dsg.commands.zmq as zmq
 
@@ -14,6 +16,20 @@ def cli():
 
 cli.add_command(visualize.cli)
 cli.add_command(zmq.cli)
+
+
+@cli.command()
+@click.argument("filepaths", nargs=-1, type=click.Path(exists=True))
+def update(filepaths):
+    """Update a collection of files."""
+    for filepath in filepaths:
+        filepath = pathlib.Path(filepath).expanduser().absolute()
+        try:
+            G = dsg.SceneGraph.load(filepath)
+            G.save(filepath, include_mesh=True)
+        except Exception as e:
+            click.secho(f"Warning: failed to convert '{filepath}': {e}")
+
 
 if __name__ == "__main__":
     cli()
