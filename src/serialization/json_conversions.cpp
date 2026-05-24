03a94ca103b345d19f7738c60ab66b7daf99da15
Nathan Hughes <nathan.h.hughes@gmail.com>
2026-05-24 09:11:13 -0400

    drop other version-based serialization checks

--- src/serialization/json_conversions.cpp
+++ src/serialization/json_conversions.cpp
@@ -118,20 +118,6 @@ void to_json(json& record, const Color& c) {
 }
 
 void from_json(const json& record, Color& c) {
-  // Support scene graphs 1.0.2 and earlier where colors were encoded as vectors.
-  // TODO(lschmid): Remove this in the future.
-  if (record.is_array()) {
-    io::warnOutdatedHeader(io::GlobalInfo::loadedHeader());
-    c.r = record.at(0).get<uint8_t>();
-    c.g = record.at(1).get<uint8_t>();
-    c.b = record.at(2).get<uint8_t>();
-    if (record.size() > 3) {
-      c.a = record.at(3).get<uint8_t>();
-    } else {
-      c.a = 255;
-    }
-    return;
-  }
   c.r = record.at("r").get<uint8_t>();
   c.g = record.at("g").get<uint8_t>();
   c.b = record.at("b").get<uint8_t>();
@@ -139,8 +125,6 @@ void from_json(const json& record, Color& c) {
     c.a = record.at("a").get<uint8_t>();
   }
 }
-
-// TODO(nathan) think about mesh serialization
 
 void to_json(json& record, const NodeAttributes& attributes) {
   serialization::Visitor::to(record, attributes);
